89c999bc961d019eefbbc0671b0edb34d0873e26
Rob Burton <synt4x.93@gmail.com>
2023-02-26 10:36:04 +0300

    security: samsung: defex_lsm: nuke

--- init/main.c
+++ init/main.c
@@ -124,11 +124,6 @@
 #include <kunit/test.h>
 #endif
 
-#ifdef CONFIG_SECURITY_DEFEX
-#include <linux/defex.h>
-void __init __weak defex_load_rules(void) { }
-#endif
-
 static int kernel_init(void *);
 
 extern void init_IRQ(void);
@@ -1695,7 +1690,4 @@ static noinline void __init kernel_init_freeable(void)
 	 */
 
 	integrity_load_keys();
-#ifdef CONFIG_SECURITY_DEFEX
-	defex_load_rules();
-#endif
 }
